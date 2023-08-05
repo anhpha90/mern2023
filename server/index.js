ac52b5f1559a845b44f75cd88df1c01e28bdc0f0
unknown <anhpha90@gmail.com>
2023-08-05 16:56:34 +0700

    df

--- server/index.js
+++ server/index.js
@@ -5,6 +5,21 @@ const mongoose = require('mongoose')
 const authRouter = require('./routes/auth')
 const postRouter = require('./routes/post')
 
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*")
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested, Content-Type, Accept Authorization"
+    )
+    if (req.method === "OPTIONS") {
+      res.header(
+        "Access-Control-Allow-Methods",
+        "POST, PUT, PATCH, GET, DELETE"
+      )
+      return res.status(200).json({})
+    }
+    next()
+  })
 const connectDB = async() => {
     try{
 await mongoose.connect(`mongodb+srv://anhpha90:Nhuquynh88@learnit.zx2ykbs.mongodb.net/?retryWrites=true&w=majority`,
@@ -24,6 +39,8 @@ process.exit(1)
 connectDB()
 const app = express()
 app.use(express.json())
+const corsOptions = { origin: ["https://heroku.com/apps/tranquil-plains-60774", "https://heroku.com/apps/tranquil-plains-60774"], credentials: true }
+app.use(cors(corsOptions));
 app.use(cors())
 app.use('/api/auth',authRouter)
 app.use('/api/posts',postRouter)
