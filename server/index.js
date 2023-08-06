6ebf21fdd54286c244099106a6d30c106726cd04
unknown <anhpha90@gmail.com>
2023-08-06 07:50:51 +0700

    asdf

--- server/index.js
+++ server/index.js
@@ -4,11 +4,9 @@ const cors = require('cors')
 const mongoose = require('mongoose')
 const authRouter = require('./routes/auth')
 const postRouter = require('./routes/post')
-
-
 const connectDB = async() => {
     try{
-await mongoose.connect(`mongodb+srv://anhpha90:Nhuquynh88@learnit.zx2ykbs.mongodb.net/?retryWrites=true&w=majority`,
+await mongoose.connect(`mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@learnit.zx2ykbs.mongodb.net/?retryWrites=true&w=majority`,
 {
     
 useNewUrlParser: true,
@@ -25,18 +23,11 @@ process.exit(1)
 connectDB()
 const app = express()
 app.use(express.json())
-const whitelist = ["http://localhost:3000"]
-const corsOptions = {
-  origin: function (origin, callback) {
-    if (!origin || whitelist.indexOf(origin) !== -1) {
-      callback(null, true)
-    } else {
-      callback(new Error("Not allowed by CORS"))
-    }
-  },
-  credentials: true,
-}
-app.use(cors(corsOptions))
+app.use(cors())
+app.get('/products/:id', function (req, res, next) {
+  res.json({msg: 'This is CORS-enabled for all origins!'})
+})
+console.log(cors)
 app.use('/api/auth',authRouter)
 app.use('/api/posts',postRouter)
 const PORT = process.env.PORT || 5000
